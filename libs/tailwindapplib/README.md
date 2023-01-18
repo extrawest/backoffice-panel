74d3549acca882bf68d6e1c0f9b6f446d386e80f
Viktor <viktor.ivanov@extrawest.com>
2023-01-18 14:45:55 +0200

    Created app for tailwind

---
+++ libs/tailwindapplib/README.md
@@ -0,0 +1,7 @@
+# tailwindapplib
+
+This library was generated with [Nx](https://nx.dev).
+
+## Running unit tests
+
+Run `nx test tailwindapplib` to execute the unit tests via [Jest](https://jestjs.io).
---
+++ libs/tailwindapplib/src/lib/containers/LoginPage/LoginPage.tsx
@@ -0,0 +1,12 @@
+import { Button } from "flowbite-react";
+import { PageLayout } from "../../layout";
+
+export const LoginPage = () => {
+	return (
+		<PageLayout>
+			<Button>sadas</Button>
+		</PageLayout>
+	);
+};
+
+export default LoginPage;
