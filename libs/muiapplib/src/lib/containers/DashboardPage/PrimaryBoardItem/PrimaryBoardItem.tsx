6f1e70007aecb73a9f5a0fe8c9ad32c5900a5928
Viktor <viktor.ivanov@extrawest.com>
2023-01-25 10:57:52 +0200

    Added minor fixes to mui app

--- libs/muiapplib/src/lib/containers/DashboardPage/PrimaryBoardItem/PrimaryBoardItem.tsx
+++ libs/muiapplib/src/lib/containers/DashboardPage/PrimaryBoardItem/PrimaryBoardItem.tsx
@@ -1,5 +1,5 @@
-import { Box, Typography } from "@mui/material";
 import { FC } from "react";
+import { Box, Typography } from "@mui/material";
 import { primaryBoardItemStyles } from "./PrimaryBoardItem.styles";
 import { PrimaryBoardItemProps } from "./PrimaryBoardItem.types";
 
